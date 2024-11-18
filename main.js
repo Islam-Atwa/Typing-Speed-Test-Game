56093e9b6026e0a784019a64b97e058c1297d448
Islam Atwa <Islam Atwa>
2024-11-19 00:23:45 +0200

    push

--- main.js
+++ main.js
@@ -1,35 +1,35 @@
 
 
 
-const RandomWord = [
+const listWord = [
     "islam", "mohamed", "code", "programing",
     "dat", "javascript",
     "testing", "Town",
     "Country",
-    "Testing",
-    "Youtube",
-    "Linkedin",
-    "Twitter",
-    "Github",
-    "Leetcode",
-    "Internet",
-    "Python",
-    "Scala",
-    "Destructuring",
-    "Paradigm",
-    "Styling",
-    "Cascade",
-    "Documentation",
-    "Coding",
-    "Funny",
-    "Working",
-    "Dependencies",
-    "Task",
-    "Runner",
-    "Roles",
-    "Test",
-    "Rust",
-    "Playing"
+    // "Testing",
+    // "Youtube",
+    // "Linkedin",
+    // "Twitter",
+    // "Github",
+    // "Leetcode",
+    // "Internet",
+    // "Python",
+    // "Scala",
+    // "Destructuring",
+    // "Paradigm",
+    // "Styling",
+    // "Cascade",
+    // "Documentation",
+    // "Coding",
+    // "Funny",
+    // "Working",
+    // "Dependencies",
+    // "Task",
+    // "Runner",
+    // "Roles",
+    // "Test",
+    // "Rust",
+    // "Playing"
 ];
 
 // Game Level
@@ -59,7 +59,7 @@ let finishMessage = document.querySelector(".finish");
 lvlNameSpan.innerHTML = defaultLevel;
 secondSpan.innerHTML = defaultLevelSeconds;
 timeLeft.innerHTML = defaultLevelSeconds;
-scoreTotal.innerHTML = RandomWord.length;
+scoreTotal.innerHTML = listWord.length;
 
 // Disable paste event
 input.addEventListener("paste", function (e) {
@@ -69,6 +69,58 @@ input.addEventListener("paste", function (e) {
 // Hide the start button after clicking it 
 startButton.onclick = function () {
     this.remove();  // Hide start button
-    input.focus();  // 
-    // Focus 
+    input.focus();  // Focus 
+    genword();
 };
+
+function genword() {
+    let RandomWord = listWord[Math.floor(Math.random() * listWord.length)];
+    let wordIndex = listWord.indexOf(RandomWord); // get word index
+    listWord.splice(wordIndex, 1); // Remove Word from Array
+
+    // show the random word
+    theWord.innerHTML = RandomWord;
+    upcomingWords.innerHTML = ''; //empty upcoming words
+
+    for (let i = 0; i < listWord.length; i++) {
+        // create div element
+        let div = document.createElement("div");
+        let txt = document.createTextNode(listWord[i]);
+        div.appendChild(txt);
+        upcomingWords.appendChild(div);
+    }
+    startPlay();
+};
+
+function startPlay(){
+    timeLeft.innerHTML = defaultLevelSeconds;
+    let start  = setInterval(()=>{
+        timeLeft.innerHTML --;
+        if (timeLeft.innerHTML === "0") {
+            // stop timer 
+            clearInterval(start);
+            if (theWord.innerHTML.toLowerCase()=== input.value.toLowerCase()){
+                input.value = ''; // reset value input
+                scoreGot.innerHTML++;
+                // التأكد من ان المصفوفة مازالت تحتوي علي كلمات
+                if (listWord.length > 0) {
+                    genword();
+                }
+                else{
+                    let span = document.createElement("span");
+                    span.className = 'good';
+                    let spanText = document.createTextNode("Congratz");
+                    span.appendChild(spanText);
+                    finishMessage.appendChild(span);
+                    // upcomingWords.remove();
+                }
+            }else{
+                let span = document.createElement("span");
+                span.className = 'bad';
+                let spanText = document.createTextNode("Game Over");
+                span.appendChild(spanText)
+                finishMessage.appendChild(span);
+            }
+        }
+    },1000);
+}
